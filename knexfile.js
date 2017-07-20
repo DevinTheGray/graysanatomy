5eb3bb4f0880df737358271863dfbbf4d16ab123
Devin Gray <devin.gray@colorado.edu>
2017-07-20 14:02:09 -0600

    yarn build

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,16 @@
 // Update with your config settings.
 
-module.exports = {
-
-  development: {
-    client: 'pg',
-    connection: 'postgresql://localhost/muscles'
-  },
-
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL
-  }
-
-};
+// module.exports = {
+//
+//   development: {
+//     client: 'pg',
+//     connection: 'postgresql://localhost/muscles'
+//   },
+//
+//
+//   production: {
+//     client: 'postgresql',
+//     connection: process.env.DATABASE_URL
+//   }
+//
+// };
